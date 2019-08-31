9e0a232379d575fc044b11ab32335cae6f19668b
Tasmia Aqila <ornicse@gmai.com>
2019-08-30 23:40:08 -0400

    Single number in array added

--- Single_Number_In_Array.java
+++ Single_Number_In_Array.java
@@ -1,27 +1,20 @@
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Single_Number_In_Array {
 
 	public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> frequency=new HashMap<Integer,Integer>();
-        for (int i=0;i<nums.length;i++){
-            if(frequency.containsKey(nums[i])){
-                frequency.put(nums[i],frequency.get(nums[i])+1);
-            }
-            else
-            {
-            	frequency.put(nums[i],1);
-            }
-        }
-         for (int i:frequency.keySet()) {
-        	 if (frequency.get(i)==1) {
-        		 return i;
-        	 }
-        	 
-         }
- 
-     return 0;
-        
+		Set<Integer> uniqueList=new HashSet<Integer>();
+		for (int i=0;i<nums.length;i++) {
+			if(uniqueList.contains(nums[i])) {
+				uniqueList.remove(nums[i]);
+			}
+			else {
+				uniqueList.add(nums[i]);
+			}
+		}
+		return uniqueList.iterator().next();
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

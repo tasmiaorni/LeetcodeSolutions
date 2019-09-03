bad19d63ea5fdc35338f287f8aa2430b71b63f7d
Tasmia Aqila <ornicse@gmai.com>
2019-09-02 23:14:25 -0400

    200. Number of Islands

--- Island.java
+++ Island.java
@@ -1,27 +1,27 @@
 public class Island {
 
-	public void DFS(int [][]graph,boolean [][]visited,int i,int j ) {
-		if(i<0 || j<0 || i==graph.length || j==graph[i].length|| visited[i][j]) {
+	public void DFS(int [][]graph,int i,int j ) {
+		if(i<0 || j<0 || i==graph.length || j==graph[i].length) {
 			return;
 		}
 		if(graph[i][j]==0)return;
-		visited[i][j]=true;
-		DFS(graph,visited,i,j+1);
-		DFS(graph,visited,i,j-1);
-		DFS(graph,visited,i+1,j);
-		DFS(graph,visited,i-1,j);
+		graph[i][j]=0;
+		DFS(graph,i,j+1);
+		DFS(graph,i,j-1);
+		DFS(graph,i+1,j);
+		DFS(graph,i-1,j);
 
 	}
 
 	public int noofislands(int [][] graph) {
 		if (graph.length == 0 || graph == null) return 0;
 		int count=0;
-		boolean [][]visited=new boolean [graph.length][graph[0].length];
+		//boolean [][]visited=new boolean [graph.length][graph[0].length];
 		for (int i=0;i<graph.length;i++) {
 			for (int j=0;j<graph[i].length;j++) {
-				if(graph[i][j]==1 && !visited[i][j]) {
+				if(graph[i][j]==1 ) {
 					count++;
-					DFS(graph,visited,i,j);
+					DFS(graph,i,j);
 
 				}
 			}
@@ -29,10 +29,10 @@ public class Island {
 		return count;
 	}
 	public static void main(String[] args) {
-		int [][]matrix= {{1,1,1,1,0},
-				{1,1,0,1,0},
+		int [][]matrix= {{1,1,0,0,0},
 				{1,1,0,0,0},
-				{0,0,0,0,0}
+				{0,0,1,0,0},
+				{0,0,0,1,1}
 		};
 		Island is=new Island();
 		System.out.print(is.noofislands(matrix));
